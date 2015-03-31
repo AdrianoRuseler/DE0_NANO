f03223adfae11a8e6b16ef377767eb07a1683aaf
Adriano Ruseler <adrianoruseler@gmail.com>
2015-03-31 16:13:36 -0300

    Alterado limite V/f para 30Hz
    Alterado limite V/f para 30Hz

--- BASE/DE0_NANO_VF/vfcontrol.vhd
+++ BASE/DE0_NANO_VF/vfcontrol.vhd
@@ -10,11 +10,13 @@ use ieee.numeric_std.all;
 entity vfcontrol is
 	generic  (
 		 constant n_bits_c: integer := 16; --numero de bits da portadora
-		 constant incMAX : std_logic_vector(12 downto 0) := std_logic_vector(to_unsigned(4832, 13)); -- Max INC for 60Hz
+	--	 constant incMAX : std_logic_vector(12 downto 0) := std_logic_vector(to_unsigned(4832, 13)); -- Max INC for 60Hz
+	    constant incMAX : std_logic_vector(12 downto 0) := std_logic_vector(to_unsigned(2416, 13)); -- Max INC for 30Hz
 		 constant incMIN : std_logic_vector(12 downto 0) := std_logic_vector(to_unsigned(483, 13));  -- Min INC for 6Hz
 		 constant I : integer := 1;  --número de bits da parte inteira excluindo sinal
 		 constant F : integer := 14; --número de bits da parte fracinária  
-		 constant mMAX : sfixed(1 downto -27) := to_sfixed(0.8137,  1, -27); -- Max modulation index 
+	--	 constant mMAX : sfixed(1 downto -27) := to_sfixed(0.8137,  1, -27); -- Max modulation index 
+	    constant mMAX : sfixed(1 downto -27) := to_sfixed(0.4069,  1, -27); -- Max modulation index 
 		 constant mMIN : sfixed(1 downto -27) := to_sfixed(0.08137,  1, -27)  -- Min modulation index 
 				);
 				
@@ -53,13 +55,11 @@ architecture vfcontrol_arch of vfcontrol is
 							incsignal <= incMAX; -- 4832 
 						   msignal <= mMAX; -- 0.8137 
 						end if;	
-				  end if;
-				  
+				  end if;				  
 		 end process;
 		
 	    inc_data <= incsignal; -- incremento do integrador
-		 m_vf <= resize(msignal,1,-14); -- Indice de Modulação
-		 
+		 m_vf <= resize(msignal,1,-14); -- Indice de Modulação	 
 		
 
 end architecture vfcontrol_arch;
